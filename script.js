d811b0313a6c3de9ae7b91319f56c1daca3cda35
mantisse <94001336+mantisse@users.noreply.github.com>
2021-12-17 19:17:46 +0200

    update

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log("Test");
+
+// 1 uzd - naudojant ciklą atspausdinti penkias eilutes ‘Labas’;
+
+for (let i = 1; i < 6; i++) {
+    console.log("Labas");
+}
+
+// 2 uzd - naudojant ciklą penkiose eilutėse atspausdinti skaičius 0 1 2 3 4 (vienas skaičius vienoje eilutėje);
+
+for (let a = 0; a<5; a++){
+    console.log(a);
+}
+console.log("----------------");
+
+
+// 3 uzd - naudojant ciklą penkiose eilutėse atspausdinti skaičius 0 10 20 30 40 (vienas skaičius vienoje eilutėje);
+
+for (let b = 0; b<5; b++){
+    console.log(b*10);  
+}
+console.log("----------------");
+
+// 4 uzd - naudojant ciklą penkiose eilutėse atspausdinti skaičius 49 50 51 52 53 (vienas skaičius vienoje eilutėje);
+for (let c = 49; c<54; c++){
+    console.log(c);
+}
+console.log("----------------");
+
+// 5 uzd - naudojant ciklą penkiose eilutėse atspausdinti skaičius 4 3 2 1 0 (vienas skaičius vienoje eilutėje);
+for (let a = 4; a>=0; a--){
+    console.log(a);
+}
+console.log("----------------");
+
+// 6 uzd - naudojant ciklą penkiose eilutėse atspausdinti skirtingus atsitiktinius skaičius nuo 0 iki 10 (vienas skaičius vienoje eilutėje);
+
