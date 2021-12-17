de87e2e01c3107deb18e82fd59f4d958621c2c7f
mantisse <94001336+mantisse@users.noreply.github.com>
2021-12-17 20:09:04 +0200

    update

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 console.log("Test");
-
+console.log("--------1--------");
 // 1 uzd - naudojant ciklą atspausdinti penkias eilutes ‘Labas’;
 
 for (let i = 1; i < 6; i++) {
     console.log("Labas");
 }
 
+console.log("--------2--------");
+
 // 2 uzd - naudojant ciklą penkiose eilutėse atspausdinti skaičius 0 1 2 3 4 (vienas skaičius vienoje eilutėje);
 
 for (let a = 0; a<5; a++){
     console.log(a);
 }
-console.log("----------------");
+
+console.log("--------3--------");
 
 
 // 3 uzd - naudojant ciklą penkiose eilutėse atspausdinti skaičius 0 10 20 30 40 (vienas skaičius vienoje eilutėje);
@@ -19,19 +22,40 @@ console.log("----------------");
 for (let b = 0; b<5; b++){
     console.log(b*10);  
 }
-console.log("----------------");
+
+console.log("--------4--------");
 
 // 4 uzd - naudojant ciklą penkiose eilutėse atspausdinti skaičius 49 50 51 52 53 (vienas skaičius vienoje eilutėje);
 for (let c = 49; c<54; c++){
     console.log(c);
 }
-console.log("----------------");
+
+console.log("--------5--------");
 
 // 5 uzd - naudojant ciklą penkiose eilutėse atspausdinti skaičius 4 3 2 1 0 (vienas skaičius vienoje eilutėje);
 for (let a = 4; a>=0; a--){
     console.log(a);
 }
-console.log("----------------");
+
+console.log("--------6--------");
 
 // 6 uzd - naudojant ciklą penkiose eilutėse atspausdinti skirtingus atsitiktinius skaičius nuo 0 iki 10 (vienas skaičius vienoje eilutėje);
 
+for (let i=0; i<5; i++){
+    let j = Math.floor(Math.random() * 11);
+    console.log(j);
+}
+
+
+// Math.floor(Math.random() * (max - min) ) + min;
+
+console.log("--------7--------");
+
+
+// 7 uzd - naudojant ciklą atspausdinti skirtingus atsitiktinius skaičius nuo 0 iki 10 (vienas skaičius vienoje eilutėje). Paskutinis atspausdintas skaičius turi būti 5;
+
+
+// for (let i=0; i<5; i++){
+//     let j = Math.floor(Math.random() * 11);
+//     console.log(j);
+}
